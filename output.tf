d23e73d8d19228aa262f3f4e970be1e0c34a1736
Raghu K <raghuk.vit@gmail.com>
2022-07-13 08:52:49 +0530

    ss

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "PRIVATE_SUBNET_IDS" {
+output "PRIVATE_SUBNETS_IDS" {
   value = aws_subnet.private-subnets.*.id
 }
 
-output "PUBLIC_SUBNET_IDS" {
+output "PUBLIC_SUBNETS_IDS" {
   value = aws_subnet.public-subnets.*.id
 }
